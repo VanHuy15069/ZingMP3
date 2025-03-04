e36b0a5e9c3918e14bca830ca38dca9031f269e3
Huy Van <HuyCV5@fpt.com>
2025-03-04 16:06:01 +0700

    fix button login

--- front-end/src/pages/loginPage/index.jsx
+++ front-end/src/pages/loginPage/index.jsx
@@ -224,9 +224,10 @@ function LoginPage() {
           </Button>
           {!isSinger && (
             <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIEND_URL}>
-              <div>
+              <div className="flex flex-col">
                 <h2 className="text-center font-bold text-[#048ec8] mb-[8px]">OR</h2>
                 <GoogleLogin
+                  ren
                   onSuccess={responseGoogle}
                   onError={() => {
                     console.log('Login Failed');
